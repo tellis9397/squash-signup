77bc860a346d29518a5c603c3e277e4eb43bc256
Thor <thornton.ellis@annkissam.com>
2018-02-21 21:23:36 -0500

    update production user mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://tv-helper-testing.herokuapp.com'
+  config.action_mailer.perform_deliveries = true
+  host = 'https://brc-squash-booking.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
